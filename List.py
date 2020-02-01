b3e827d8123ec59bd82bf8b59fe96fa2e8ae307d
MyatMinKhant23 <myat.khant23@gmail.com>
2020-02-01 12:05:44 +0630

    list.py

---
+++ List.py
@@ -0,0 +1,40 @@
+word = 'Programming'
+
+  P   r  o  g  r  a  m  m  i  n  g
+  0   1  2  3  4  5  6  7  8  9  10
+-11 -10 -9 -8 -7 -6 -5 -4 -3 -2 -1
+
+>>> Word = 'Programming'
+>>> Word
+'Programming'
+>>> Word[0]
+'P'
+>>> Word[7]
+'m'
+>>> Word[-2]
+'n'
+>>> Word[3:5]
+'gr'
+>>> Word[0:-11]
+''
+>>> Word[0:-10]
+'P'
+>>> Word[0:]
+'Programming'
+>>> Word[3:]
+'gramming'
+>>> Word[0:-7]
+'Prog'
+>>> Word[:2] + Word[3:]
+'Prgramming'
+
+len(word)
+
+# len = length
+
+square = 'Square'
+len(square)
+len(Word) + len(square)
+
+>>> cube = [10, 20, 30, 45, 50]
+>>> cube
