1a1a4def72a3dc2236784ecae913cdf11aeed9cd
MyatMinKhant23 <myat.khant23@gmail.com>
2020-03-06 12:12:26 +0630

    fibonacci,Result,Student,Mary

---
+++ Student/result.py
@@ -0,0 +1,23 @@
+import student
+import mary
+
+x = student.a["code"]
+y = student.b["code"]
+z = student.a["student"]
+
+print(x, ' ', y, ' ', z)
+
+for i in range(len(mary.a)):
+	print(i, mary.a[i])
+
+z = student.a["student"]
+m = student.a["age"]
+c = student.a["mother"]
+v = student.b["student"]
+n = student.b["father"]
+
+print(z, ' ', m, ' ', c, ' ', v, ' ', n )
+
+num = int(input("enter the number of rows:"))
+for i in range(1,num+1):
+	print("* "*i)
