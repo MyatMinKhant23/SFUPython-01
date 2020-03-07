6b1b7f6218b3b8657faf6f10ad4f939b210accba
MyatMinKhant23 <myat.khant23@gmail.com>
2020-03-07 12:07:18 +0630

    PIP

---
+++ result.py
@@ -0,0 +1,20 @@
+import student
+import marym
+
+x = student.a["code"]
+y = student.b["code"]
+z = student.a["student"]
+
+print(x, ' ', y, ' ', z)
+
+for i in range(len(marym.a)):
+	print(i, marym.a[i])
+
+z = student.a["student"]
+m = student.a["age"]
+c = student.a["mother"]
+v = student.b["student"]
+n = student.b["father"]
+
+print(z, ' ', m, ' ', c, ' ', v, ' ', n )
+
