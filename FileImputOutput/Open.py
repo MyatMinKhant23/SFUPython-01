deb3f23d477bc030476b31b403916d256d255a50
MyatMinKhant23 <myat.khant23@gmail.com>
2020-03-13 12:06:25 +0630

    FileInputOutput

---
+++ FileImputOutput/Open.py
@@ -0,0 +1,52 @@
+# # "r" - Read
+
+# # "a" - Append
+
+# # "w" - Write
+
+# # "x" - Create
+
+# # "t" - Text
+
+# # "b" - Binary 
+
+# # open & Read file
+
+# # f = open('test.txt','r') # R - read
+# # print(f.name)
+# # f.close()
+
+# # g = open('test1.txt', 'a') 
+# # g.write('This is line number '+"\n")
+# # print (g,end='')
+
+
+# with open('test.txt', 'r') as f:
+
+# 	# f_text = f.readline()
+# 	# print(f_text,end='')
+
+
+# 	# f_text = f.readline()
+# 	# print(f_text,end='')
+
+# 	# f_text = f.readline()
+# 	# print(f_text,end=' ')
+
+# 	# for f_text in f:
+# 	# 	print(f_text,end='')
+
+# 		# f_text = f.read(60)
+# 		# print(f_text,end='')
+
+
+# 		# f_text = f.read(100)
+
+# 		# while 100> 0:
+# 		# 	print(f_text,end='')
+
+
+# # import 
+# # os.remove(testCOPY.txt)
+
+# # #Worl with test.txt
---
+++ FileImputOutput/parse_csv.py
@@ -0,0 +1,36 @@
+# import csv
+
+# with open('names.csv', 'r') as csv_file:
+# 	csv_reader = csv.DictReader(csv_file)
+
+# 	with open('new_names.csv', 'w') as new_file:
+# 		fieldnames = ['ID', 'first_name', 'last_name', ]
+
+# 		csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames, delimiter='\t')
+
+# 		csv_writer.writeheader()
+
+# 		for line in csv_reader:
+# 			del line['email']
+# 			csv_writer.writerow(line)
+
+
+import csv
+
+with open('create_names.csv', 'r') as csv_file:
+	csv_reader = csv.DictReader(csv_file)
+
+	with open('save_names.csv', 'w') as new_file:
+		fieldnames = ['ID', 'first_name', 'last_name',]
+
+		csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames, delimiter='\t')
+
+		csv_writer.writeheader()
+
+		for line in csv_reader:
+			del line['email']
+			csv_writer.writerow(line)
+
+
+
+
